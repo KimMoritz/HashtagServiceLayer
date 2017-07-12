27a19da0b8f9575473aba75453ae94e49beccff2
kim <kim.moritz@hotmail.com>
2017-07-12 16:39:09 +0200

    Removed @Component

--- src/main/java/mongo/MongoRequester.java
+++ src/main/java/mongo/MongoRequester.java
@@ -3,6 +3,8 @@ package mongo;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.stereotype.Component;
+
 import javax.jms.*;
 
 public class MongoRequester {
